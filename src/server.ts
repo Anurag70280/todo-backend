1b1e3149a93463eb2bf893297350404bed8a7788
Your Name <anurag6009singh@gmail.com>
2025-11-04 21:08:23 +0530

    chnaged mongo uri

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 import app from './app';
 
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI || '';
+const MONGO_URI = process.env.MONGODB_URI || '';
 
 const startServer = async () => {
     try {
-        await mongoose.connect(MONGODB_URI);
+        await mongoose.connect(MONGO_URI);
         console.log('MongoDB connected');
 
         app.listen(PORT, () => {
